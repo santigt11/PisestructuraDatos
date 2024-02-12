55015c1764e5eceaca6f3054c8a25b3a8321e782
Estefania <estefania.torres@unl.edu.ec>
2024-02-12 15:30:04 -0500

    Arreglo Frm Informes

--- src/vista/FrmTutoriasPrincipal.java
+++ src/vista/FrmTutoriasPrincipal.java
@@ -322,7 +322,7 @@ public class FrmTutoriasPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 894, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
